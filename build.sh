84dc94e676794e62b99630ab7b991b460b2088db
mastercoms <mastercoms@tuta.io>
2020-12-25 13:04:18 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 # install mingw-w64 for unarr
-export CGO_ENABLED=1
-GOARCH=amd64 GOOS=windows CC=x86_64-w64-mingw32-gcc go build -ldflags '-w'
+CGO_ENABLED=1 GOARCH=amd64 GOOS=windows CC=x86_64-w64-mingw32-gcc go build -ldflags '-w'
